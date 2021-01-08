c395f8017bcd27e95bfa95c3b67eb540f5d4c024
mshogren <3902009+mshogren@users.noreply.github.com>
2021-01-08 14:51:10 -0700

    fix org path for new server

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -28,7 +28,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory (if (file-directory-p "~/org/") "~/org/" "/ssh:dev:./Dropbox/"))
+(setq org-directory (if (file-directory-p "~/org/") "~/org/" "/ssh:mndev:./org/"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
